a335aa20f7b06f4a62e9873d041ea0aa1b69224a
Rachid Abadli <rachid@sac.associates>
2024-04-14 13:11:49 -0700

    Another test

--- frontend/src/containers/Accounts.js
+++ frontend/src/containers/Accounts.js
@@ -5,6 +5,10 @@ import { View, Heading, Flex } from '@aws-amplify/ui-react';
 import { getItems as GetItems } from '../graphql/queries.js';
 import Plaid from '../components/Plaid';
 import Institutions from '../components/Institutions';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 
 const logger = new ConsoleLogger("Protected");
 
@@ -27,6 +31,29 @@ export default function   Protected() {
   useEffect(() => {
     getItems();
   }, []);
+  const card = (
+    <React.Fragment>
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          Word of the Day
+        </Typography>
+        <Typography variant="h5" component="div">
+          be{bull}nev{bull}o{bull}lent
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          adjective
+        </Typography>
+        <Typography variant="body2">
+          well meaning and kindly.
+          <br />
+          {'"a benevolent smile"'}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Learn More</Button>
+      </CardActions>
+    </React.Fragment>
+  );
 
   return (
     <Flex direction="column">
@@ -34,7 +61,9 @@ export default function   Protected() {
       {(items && items.length) ? (
         <View>
           <Heading>Institutions</Heading>
-          <p>Account Found!!</p>
+          <Box sx={{ minWidth: 275 }}>
+            <Card variant="outlined">{card}</Card>
+          </Box>
           <Institutions institutions={items}/>
         </View>
       ) : (<div/>)
