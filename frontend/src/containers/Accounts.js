61347db3b57d1183934ceae45f82220c565e391b
Rachid Abadli <rachid@sac.associates>
2024-04-14 13:43:49 -0700

    Added Card and message

--- frontend/src/containers/Accounts.js
+++ frontend/src/containers/Accounts.js
@@ -10,7 +10,7 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { React } from 'react';
+import * as React from 'react';
 import Button from '@mui/material/Button';
 
 
